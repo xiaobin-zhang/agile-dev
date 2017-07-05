0e47a40acf1a57406b3765a70af6ba74c227ec5b
zhangxiaobin <zhangxiaobin@zhangxiaobin-PC>
2017-07-05 16:51:21 +0800

    yy

---
+++ src/main/java/com/agile/demo/demo2/CurrencyMain.java
@@ -0,0 +1,14 @@
+package com.agile.demo.demo2;
+
+public class CurrencyMain {
+
+	public static void main(String[] args) {
+		test1();
+	}
+	
+	public static void test1(){
+		Currency currency = new Currency(1);
+		String formate = currency.formate(100);
+		System.out.println(formate);
+	}
+}
